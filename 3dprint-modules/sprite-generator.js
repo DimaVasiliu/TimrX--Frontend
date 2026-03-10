e3841de8ba29767125ae38abbba71745f62fb15e
Dima <120177257+DimaVasiliu@users.noreply.github.com>
2026-03-10 13:45:04 +0000

    Update sprite-generator.js

--- 3dprint-modules/sprite-generator.js
+++ 3dprint-modules/sprite-generator.js
@@ -259,6 +259,10 @@
   // =========================================================================
 
   function downloadDataUrl(dataUrl, filename) {
+    if (!window.WorkspaceCredits?.canDownloadAssets?.()) {
+      if (confirm('You need credits to download assets.\n\nWould you like to get credits?')) window.location.href = 'hub.html#pricing';
+      return;
+    }
     const link = document.createElement('a');
     link.href = dataUrl;
     link.download = filename;
